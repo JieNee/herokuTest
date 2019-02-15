fa8d3929f7c7641dfe08121d68d83f927b340a51
JieNee <jerry89084@gmail.com>
2019-02-15 12:54:54 +0800

    init

--- herokuTest/Controllers/LineController.cs
+++ herokuTest/Controllers/LineController.cs
@@ -8,11 +8,12 @@ using Microsoft.AspNetCore.Mvc;
 namespace herokuTest.Controllers
 {
     [Produces("application/json")]
-    [Route("api/Line")]
+    
     public class LineController : Controller
     {
+        [Route("api/Line")]
         [HttpPost]
-        public ActionResult webhook()
+        public ActionResult webhook(string x="")
         {
             string ChannelAccessToken = "PQ8Lm5Wj/R1dD3KO4crlrEfeKN7axCE51n+1Ww61gT2YxxhmvFCkb3GxxlNU8pAth2vWYdkqg6y3OF6YUJJUerEktH+BGA/1DD0tcEFI5heQWLQx1SjUBDDpaAB2AOI9Yp5RsbptVn9fKFcGOX0OrAdB04t89/1O/w1cDnyilFU=";
 
@@ -32,7 +33,33 @@ namespace herokuTest.Controllers
             }
             catch (Exception ex)
             {
+                return Ok(ex.Message);
+            }
+        }
+
+        [Route("api/Line2")]
+        [HttpPost]
+        public ActionResult qwe(string x = "")
+        {
+            string ChannelAccessToken = "PQ8Lm5Wj/R1dD3KO4crlrEfeKN7axCE51n+1Ww61gT2YxxhmvFCkb3GxxlNU8pAth2vWYdkqg6y3OF6YUJJUerEktH+BGA/1DD0tcEFI5heQWLQx1SjUBDDpaAB2AOI9Yp5RsbptVn9fKFcGOX0OrAdB04t89/1O/w1cDnyilFU=";
+
+            try
+            {
+                //取得 http Post RawData(should be JSON)
+                string postData = Request.Body.ToString();
+                //剖析JSON
+                var ReceivedMessage = isRock.LineBot.Utility.Parsing(postData);
+                //回覆訊息
+                string Message;
+                Message = "你說了:" + ReceivedMessage.events[0].message.text;
+                //回覆用戶
+                isRock.LineBot.Utility.ReplyMessage(ReceivedMessage.events[0].replyToken, Message, ChannelAccessToken);
+                //回覆API OK
                 return Ok();
+            }
+            catch (Exception ex)
+            {
+                return Ok(ex.Message);
             }
         }
     }
